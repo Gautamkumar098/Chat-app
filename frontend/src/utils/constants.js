06e3f5207cbbb9b3ac1a93f1957d4faae456ecad
Gautam kumar <gauta7318@gmail.com>
2026-07-11 21:31:35 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://chat-app-fcc5.onrender.com";
 
 export const API_URL_USER = `${API_URL}/api/user`;
 export const API_URL_MESSAGE = `${API_URL}/api/message`;
